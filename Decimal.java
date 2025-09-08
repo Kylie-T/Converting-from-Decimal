4e8e8d3be14ec4769447ee5382c7c353bb319339
Kylie-T <kyliebellet@gmail.com>
2025-09-08 11:52:09 -0500

    Update Decimal.java

--- Decimal.java
+++ Decimal.java
@@ -4,28 +4,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class DecimalTest {
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number");
-        double num = scanner.nextFloat();
-
-        System.out.println("Enter the base");
-        int base = scanner.nextInt();
-
-        Decimal conversion = new Decimal(num, base);
-
-        conversion.conIntBack();
-        for(int i = 0; i < conversion.conInt().size(); i++) {
-            System.out.print(conversion.conInt().get(i));
-        }
-        System.out.print(".");
-        for(int i = 0; i < conversion.conDec().size(); i++) {
-            System.out.print(conversion.conDec().get(i));
-        }
-    }
-}
 class Decimal {
 
     private double num;
