fe4a0c2abdd104f5cca8dc886d1b313f82d67767
Kylie-T <kyliebellet@gmail.com>
2025-09-10 16:36:25 -0500

    Update DecimalTest.java

--- DecimalTest.java
+++ DecimalTest.java
@@ -1,29 +1,60 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class DecimalTest {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number");
-        double num = scanner.nextDouble();
+        boolean mayRun = true;
 
-        System.out.println("Enter the base");
-        int base = scanner.nextInt();
+        while (mayRun) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a number");
+            double num = scanner.nextDouble();
 
-        Decimal conversion = new Decimal(num, base);
+            System.out.println("Enter the base");
+            int base = scanner.nextInt();
 
-        conversion.conIntBack();
-        for(int i = 0; i < conversion.conInt().size(); i++) {
-            System.out.print(conversion.conInt().get(i));
-        }
-        System.out.print(".");
+            Decimal conversion = new Decimal(num, base);
+            ArrayList<Integer> convertInt = conversion.conInt();
+            ArrayList<Integer> convertFrac = conversion.conDec();
 
-        for (int i = 0; i < conversion.conDec().size(); i++) {
-            System.out.print(conversion.conDec().get(i));
-        }
+            for (int numInt : convertInt) {
+                if (numInt < 10) {
+                    System.out.print(numInt);
+                } else {
+                    for (int i = 0; i < conversion.hexNum.length;i++) {
+                        if (conversion.hexNum[i] == numInt) {
+                            System.out.print(conversion.hexLet[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (!convertFrac.isEmpty()) {
+                System.out.print(".");
+                for (int decimalNum : convertFrac) {
+                    if (decimalNum < 10) {
+                        System.out.print(decimalNum);
+                    } else {
+                        for (int i = 0; i < conversion.hexNum.length;i++) {
+                            if (conversion.hexNum[i] == decimalNum) {
+                                System.out.print(conversion.hexLet[i]);
+                                break;
+                            }
+                        }
+                    }
+                }
 
-        if (conversion.conDec().size() > 10) {
-            System.out.print("...");
+                if (convertFrac.size() > 10) {
+                    System.out.print("...");
+                }
+                System.out.println("\nAnother? (type n or y)");
+                Scanner scannerAns = new Scanner(System.in);
+                String answer = scannerAns.nextLine();
+                if (answer.charAt(0) == 'n') {
+                    mayRun = false;
+                }
+            }
         }
     }
 }
